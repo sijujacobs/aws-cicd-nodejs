0558ce556234eb2bfc2c506b284d5532562d414d
Siju Jacob <sijujacob@Sijus-MacBook-Pro-2.local>
2021-11-24 13:05:44 -0500

    fix - port number

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('<h2>Welcome to NodeJS-AWS-CICD-APP</h2><h3>Success</h3>')
