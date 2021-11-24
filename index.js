8e4dc1834e537419a9ecca462673fcda27b69ca6
Siju Jacob <sijujacob@Sijus-MacBook-Pro-2.local>
2021-11-24 10:39:14 -0500

    aws-nodejs-cicd-app initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('<h2>Welcome to NodeJS-AWS-CICD-APP</h2><h3>Success</h3>')
+});
+
+app.get('/products', (req, res) => {
+    res.send([
+        {
+            productId: '1001',
+            price: '550',
+            color: 'White'
+        },
+        {
+            productId: '1002',
+            price: '660',
+            color: 'Black'
+        },
+        {
+            productId: '1003',
+            price: '770',
+            color: 'Green'
+        }
+    ])
+});
+
+app.listen(port, () => {
+    console.log(`NodeJS-AWS-CICD-APP is UP and LISTENING to port :${port}`);
+})
